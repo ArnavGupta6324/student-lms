886df918e7c2a2590ba78d1cc39aa9fa37ebde2d
Suryanshi247 <thappasuryanshi@gmail.com>
2024-12-05 20:27:17 +0530

    student page structure

---
+++ lms-admin-panel/pages/student/index.js
@@ -0,0 +1,15 @@
+import StudentNavbar from '../../student-page/components/StudentNavbar';
+
+const Dashboard = () => {
+    return (
+        <>
+            <StudentNavbar />
+            <div className="dashboard">
+                <h1>Welcome to the Student Dashboard</h1>
+                <p>Manage your courses, view history, and leave reviews.</p>
+            </div>
+        </>
+    );
+};
+
+export default Dashboard;
